5d82fef8e6326119a683d9b9ce496c046e923ef4
aarti <sharmaaarti50528@gmail.com>
2024-09-11 16:49:07 +0530

    --updated hyperparameters of a2c and ddpg

--- finrl/config.py
+++ finrl/config.py
@@ -31,14 +31,21 @@ INDICATORS = [
 
 
 # Model Parameters
-A2C_PARAMS = {"n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
+A2C_PARAMS = {
+    # "n_steps": 5,
+    "n_steps": 10, 
+    "ent_coef": 0.01, 
+    "learning_rate": 0.0007}
 PPO_PARAMS = {
     "n_steps": 2048,
     "ent_coef": 0.01,
     "learning_rate": 0.00025,
     "batch_size": 64,
 }
-DDPG_PARAMS = {"batch_size": 128, "buffer_size": 50000, "learning_rate": 0.0007}
+DDPG_PARAMS = {"batch_size": 128, "buffer_size": 50000, 
+               "learning_rate": 0.0003
+            #    "learning_rate": 0.0007
+               }
 TD3_PARAMS = {"batch_size": 100, "buffer_size": 1000000, "learning_rate": 0.001}
 SAC_PARAMS = {
     "batch_size": 64,
